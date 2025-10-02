66c658496300b71876a5f46eb6d85501863d8f88
Abcom <tanveer.rayeen@omnicoredigital.com>
2025-10-02 17:36:55 +0530

    added test cases

---
+++ src/test/java/com/dukhan/forgot/adapter/api/ForgotPasswordApplicationTest.java
@@ -0,0 +1,13 @@
+package com.dukhan.forgot.adapter.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ForgotPasswordApplicationTest {
+
+    @Test
+    void contextLoad() {
+
+    }
+}
